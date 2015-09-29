310c1848b83fc56183466eedb53451b62d299b2b
ileeja00 <ehdwjd94@naver.com>
2015-09-29 23:01:30 +0900

    Update hw1.java

--- hw1.java
+++ hw1.java
@@ -13,6 +13,20 @@ public PhoneInfo(String name, String phone_number, String group, String brithday
  this.birthday = birthday;
 }
 
+public PhoneInfo(String name, String phone_number, String group){
+ this.name = name;
+ this.phone_number = phone_number;
+ this.group = group;
+ this.birthday = null;
+}
+
+public PhoneInfo(String name, String phone_number, String birthday){
+ this.name = name;
+ this.phone_number = phone_number;
+ this.group = null;
+ this.birthday = birthday;
+}
+
 public PhoneInfo(String name, String phone_number){
  this.name = name;
  this.phone_number = phone_number;
