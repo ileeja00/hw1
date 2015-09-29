8fe5ec88b99bca3497df686bdb7b43c8783460ab
ileeja00 <ehdwjd94@naver.com>
2015-09-29 22:27:38 +0900

    Update hw1.java

---
+++ hw1.java
@@ -0,0 +1,32 @@
+
+class PhoneInfo{
+
+private String name;
+private String phone_number;
+private String group;
+private String birthday;
+
+public PhoneInfo(String name, String phone_number, String group, String brithday){
+ this.name = name;
+ this.phone_number = phone_number;
+ this.group = group;
+ this.birthday = birthday;
+}
+
+public PhoneInfo(String name, String phone_number){
+ this.name = name;
+ this.phone_number = phone_number;
+ this.group = null;
+ this.birthday = null;
+}
+
+public void showInfo(){
+ System.out.println(name+"\n"+phone_number);
+ if(group!=null)
+  System.out.println(group);
+ if(birthday!=null)
+  System.out.println(birthday);
+
+ System.out.println();
+ }
+ }
